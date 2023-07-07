70e4e9abe415582bea076a824de2ab016ab1e9ec
caleb <shawcaleb1688@gmail.com>
2023-07-07 16:43:20 -0400

    made my axios request and characters is now the characters array in the api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([])
+
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(results => {
+      setCharacters(results.data)
+      console.log('Characters:',characters)
+      
+    })
+    .catch(err => console.log(err))
+  },[])
+
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -10,7 +25,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">REACT WARS</h1>
     </div>
   );
 }
